b9070ca87639ccb27d105c6622d314912060e2bd
José Mendes <61622582+zemendes1@users.noreply.github.com>
2023-03-23 22:23:10 +0000

    main + opcua

--- MES/pythonProject/main.py
+++ MES/pythonProject/main.py
@@ -4,5 +4,10 @@ from PyroClient import *
 # run_gui()
 # run_pyro_client()
 
-# exec(open("opcua.py").read())
+# Experiência
+In_bool_value = None
+In_uint_value = None
+Out_uint_value = 70
+Out_bool_value = True
 
+# exec(open("opcua.py").read())
--- MES/pythonProject/opcua.py
+++ MES/pythonProject/opcua.py
@@ -1,31 +1,32 @@
+import main as Mn
 import asyncio
 from asyncua import Client
 from asyncua import ua
 
-url = "opc.tcp://127.0.0.1:4840/"
 
-global bool_value
-global uint_value
+url = "opc.tcp://127.0.0.1:4840/"
 
 
 async def main():
+    print(Mn.Out_bool_value)
+    print(Mn.Out_uint_value)
     async with Client(url=url) as client:
         while True:
             # Read Boolean Value
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Test")
-            bool_value = await node.read_value()
+            Mn.In_bool_value = await node.read_value()
 
             # Read Uint16 Value
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.vetor[0]")
-            uint_value = await node.read_value()
+            Mn.In_uint_value = await node.read_value()
 
             # Write Boolean Value
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Test")
-            await node.write_value(ua.Variant(True, ua.VariantType.Boolean))
+            await node.write_value(ua.Variant(Mn.Out_bool_value, ua.VariantType.Boolean))
 
             # Write Uint16 Value
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.vetor[0]")
-            await node.write_value(ua.Variant(69, ua.VariantType.UInt16))
+            await node.write_value(ua.Variant(Mn.Out_uint_value, ua.VariantType.UInt16))
 
 
 if __name__ == "__main__":
