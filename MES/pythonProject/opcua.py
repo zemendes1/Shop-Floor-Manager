ca624082fc6c6cd6eabcddc1768905e911347504
Jose Miguel Mendes <up201906869@up.pt>
2023-03-23 19:15:07 +0000

    update opcua

--- MES/pythonProject/opcua.py
+++ MES/pythonProject/opcua.py
@@ -4,6 +4,9 @@ from asyncua import ua
 
 url = "opc.tcp://127.0.0.1:4840/"
 
+global bool_value
+global uint_value
+
 
 async def main():
     async with Client(url=url) as client:
@@ -12,17 +15,17 @@ async def main():
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Test")
             bool_value = await node.read_value()
 
-            # Write Boolean Value
-            new_value = not bool_value
-            await node.write_value(ua.Variant(new_value, ua.VariantType.Boolean))
-
             # Read Uint16 Value
             node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.vetor[0]")
             uint_value = await node.read_value()
 
+            # Write Boolean Value
+            node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.Test")
+            await node.write_value(ua.Variant(True, ua.VariantType.Boolean))
+
             # Write Uint16 Value
-            new_value = uint_value + 1
-            await node.write_value(ua.Variant(new_value, ua.VariantType.UInt16))
+            node = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.vetor[0]")
+            await node.write_value(ua.Variant(69, ua.VariantType.UInt16))
 
 
 if __name__ == "__main__":
