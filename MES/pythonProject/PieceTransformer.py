fcb3def12a3dda42d12b9da64bc65fb2b3389f28
José Mendes <61622582+zemendes1@users.noreply.github.com>
2023-03-23 11:52:20 +0000

    Define function with transform vector

--- MES/pythonProject/PieceTransformer.py
+++ MES/pythonProject/PieceTransformer.py
@@ -1,131 +1,153 @@
 pathVector = []  # Categorias de transformação que a peça de origem tem de alcançar para se transformar na peça pedida
 
-OrderPiece = 'P7_from_P2'  # Peça Pedida
 
-if OrderPiece == 'P3_from_P2':
-    pathVector.append(3)
-    pathVector.append(2)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+# Casos Possívies
+# P3_from_P2
+# P4_from_P2
+# P5_from_P2
+# P5_from_P4
+# P5_from_P7
+# P5_from_P9
+# P6_from_P1
+# P6_from_P2
+# P6_from_P3
+# P7_from_P2
+# P7_from_P4
+# P8_from_P1
+# P8_from_P2
+# P8_from_P3
+# P8_from_P6
+# P9_from_P2
+# P9_from_P4
+# P9_from_P7
 
-if OrderPiece == 'P4_from_P2':
-    pathVector.append(4)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+def define_vector(OrderPiece):
+    if OrderPiece == 'P3_from_P2':
+        pathVector.append(3)
+        pathVector.append(2)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P5_from_P2':
-    pathVector.append(5)
-    pathVector.append(4)
-    pathVector.append(4)
-    pathVector.append(3)
-    pathVector.append(4)
+    if OrderPiece == 'P4_from_P2':
+        pathVector.append(4)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P5_from_P4':
-    pathVector.append(5)
-    pathVector.append(4)
-    pathVector.append(3)
-    pathVector.append(4)
-    pathVector.append(0)
+    if OrderPiece == 'P5_from_P2':
+        pathVector.append(5)
+        pathVector.append(4)
+        pathVector.append(4)
+        pathVector.append(3)
+        pathVector.append(4)
 
-if OrderPiece == 'P5_from_P7':
-    pathVector.append(5)
-    pathVector.append(3)
-    pathVector.append(4)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P5_from_P4':
+        pathVector.append(5)
+        pathVector.append(4)
+        pathVector.append(3)
+        pathVector.append(4)
+        pathVector.append(0)
 
-if OrderPiece == 'P5_from_P9':
-    pathVector.append(5)
-    pathVector.append(4)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P5_from_P7':
+        pathVector.append(5)
+        pathVector.append(3)
+        pathVector.append(4)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P6_from_P1':
-    pathVector.append(6)
-    pathVector.append(1)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P5_from_P9':
+        pathVector.append(5)
+        pathVector.append(4)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P6_from_P2':
-    pathVector.append(6)
-    pathVector.append(2)
-    pathVector.append(1)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P6_from_P1':
+        pathVector.append(6)
+        pathVector.append(1)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P6_from_P3':
-    pathVector.append(6)
-    pathVector.append(1)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P6_from_P2':
+        pathVector.append(6)
+        pathVector.append(2)
+        pathVector.append(1)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P7_from_P2':
-    pathVector.append(7)
-    pathVector.append(3)
-    pathVector.append(4)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P6_from_P3':
+        pathVector.append(6)
+        pathVector.append(1)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P7_from_P4':
-    pathVector.append(7)
-    pathVector.append(4)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P7_from_P2':
+        pathVector.append(7)
+        pathVector.append(3)
+        pathVector.append(4)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P8_from_P1':
-    pathVector.append(8)
-    pathVector.append(1)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P7_from_P4':
+        pathVector.append(7)
+        pathVector.append(4)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P8_from_P2':
-    pathVector.append(8)
-    pathVector.append(2)
-    pathVector.append(1)
-    pathVector.append(3)
-    pathVector.append(0)
+    if OrderPiece == 'P8_from_P1':
+        pathVector.append(8)
+        pathVector.append(1)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P8_from_P3':
-    pathVector.append(8)
-    pathVector.append(1)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P8_from_P2':
+        pathVector.append(8)
+        pathVector.append(2)
+        pathVector.append(1)
+        pathVector.append(3)
+        pathVector.append(0)
 
-if OrderPiece == 'P8_from_P6':
-    pathVector.append(8)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P8_from_P3':
+        pathVector.append(8)
+        pathVector.append(1)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P9_from_P2':
-    pathVector.append(9)
-    pathVector.append(3)
-    pathVector.append(4)
-    pathVector.append(3)
-    pathVector.append(0)
+    if OrderPiece == 'P8_from_P6':
+        pathVector.append(8)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
 
-if OrderPiece == 'P9_from_P4':
-    pathVector.append(9)
-    pathVector.append(4)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P9_from_P2':
+        pathVector.append(9)
+        pathVector.append(3)
+        pathVector.append(4)
+        pathVector.append(3)
+        pathVector.append(0)
 
-if OrderPiece == 'P9_from_P7':
-    pathVector.append(9)
-    pathVector.append(3)
-    pathVector.append(0)
-    pathVector.append(0)
-    pathVector.append(0)
+    if OrderPiece == 'P9_from_P4':
+        pathVector.append(9)
+        pathVector.append(4)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
 
+    if OrderPiece == 'P9_from_P7':
+        pathVector.append(9)
+        pathVector.append(3)
+        pathVector.append(0)
+        pathVector.append(0)
+        pathVector.append(0)
+
+
+define_vector('P7_from_P2')
 print(pathVector)
