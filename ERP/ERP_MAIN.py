f2fc2618d8db878133c7a2de35883331a6dc880d
José Mendes <61622582+zemendes1@users.noreply.github.com>
2023-05-25 16:13:17 +0100

    ERP_MAIN fix

--- ERP/ERP_MAIN.py
+++ ERP/ERP_MAIN.py
@@ -5,15 +5,17 @@ from ERP import MPS
 from ERP import receive_xml
 
 
+def execute_everything_else():
+    receive_xml.run_xml()
+
+
 def execute_interface():
     interfaceErp.setup_interface()
 
 
-def execute_everything_else():
-    while True:
-
-        MPS.continuous_processing()
-        receive_xml.run_xml()
+def execute_mps():
+    # MPS.continuous_processing()
+    pass
 
 
 if __name__ == '__main__':
@@ -22,13 +24,16 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
 
     # create processes for each function
+    process3 = multiprocessing.Process(target=execute_everything_else)
     process1 = multiprocessing.Process(target=execute_interface)
-    process2 = multiprocessing.Process(target=execute_everything_else)
+    process2 = multiprocessing.Process(target=execute_mps)
 
     # start the processes
     process1.start()
     process2.start()
+    process3.start()
 
     # wait for the processes to finish
     process1.join()
     process2.join()
+    process3.join()
